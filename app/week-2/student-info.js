85aceef417c7d08bb1a8021f01af119188f18157
OwenC03 <owen.carson@edu.sait.ca>
2024-09-04 22:18:17 -0600

    Week 2 early push

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <p>Owen Steven Carson</p>
+            <div>
+                <Link href="https://github.com/OwenC03/cprg306-assignments">
+                    Press here for Github
+                </Link>
+            </div>
+        </main>
+    );
+}
