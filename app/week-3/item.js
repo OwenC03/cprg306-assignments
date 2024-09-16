26b40e402f7544241cf3f2ac9f8cacea38d8604a
OwenC03 <owen.carson@edu.sait.ca>
2024-09-16 17:21:46 -0600

    week 3 final edit

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,13 +1,13 @@
-
-
-export default function Item({name, quantity, category}){
-    return(
-        <li>
-            <div>
-                <h3 className="text-base text-red-600">{name}</h3>
-                <h3 className="text-base text-orange-600">Category: {category}</h3>
-                <h3 className="text-base text-red-600">Quantity: {quantity}</h3>
-            </div>
-        </li>
-    );
+export default function Item({ name, quantity, category }) {
+  return (
+    <li className="p-4 m-4 bg-slate-800 max-w-sm rounded-lg flex justify-between items-center">
+      <div>
+        <h2 className="text-xl font-semibold text-red-600">{name}</h2>
+        <div className="text-orange-600">
+          Buy {quantity} in the {category} section
+        </div>
+      </div>
+      <input type="checkbox" className="h-6 w-6"/>
+    </li>
+  );
 }
