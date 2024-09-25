a3833b41af74cdb18c8197345ea26af634e88120
OwenC03 <owen.carson@edu.sait.ca>
2024-09-25 10:18:09 -0600

    update to week 4

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,11 +1,10 @@
-
 import NewItem from "./new-item";
 
-export default function Page(){
-    return(
-        <div>
-            <h1 className="text-center text-3xl">Week 4 Page</h1>
-            <NewItem />
-        </div>
-    )
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-center text-3xl">Week 4 Page</h1>
+      <NewItem />
+    </div>
+  );
 }
